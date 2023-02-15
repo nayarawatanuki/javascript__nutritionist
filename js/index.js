dbbb0d3ac259044801d97b692feefe34502bcebb
nayarawatanuki <nayarawatanuki@gmail.com>
2023-02-15 01:30:43 -0300

    js: codigo organizado e calc. aplicado p/ toda tabela

--- js/index.js
+++ js/index.js
@@ -2,33 +2,33 @@
 var title = document.querySelector(".title");
 title.textContent = "Nutrição";
 
-var patients = document.querySelector("patient");
+var patients = document.querySelectorAll(".patient");
+console.log(patients);
 
-var patient = document.querySelector("#first-patient");
-var weight = document.querySelector(".patient__weight").textContent;
-var height = document.querySelector(".patient__height").textContent;
-var imc = document.querySelector(".patient__imc");
+for(var i = 0; i < patients.length; i++) {
+    var patient = patients[i];
 
-var weightValid = true;
-var heightValid = true;
+    var weight = patient.querySelector(".patient__weight").textContent;
+    var height = patient.querySelector(".patient__height").textContent;
+    var imc = patient.querySelector(".patient__imc");
 
-if(weight <= 0 || weight >= 1000) {
-    console.log("Peso inválido.");
-    weightValid = false;
-    imc.textContent = "Peso inválido!"
+    var weightValid = true;
+    var heightValid = true;
+
+    if(weight <= 0 || weight >= 1000) {
+        console.log("Peso inválido.");
+        weightValid = false;
+        imc.textContent = "Peso inválido!"
+    }
+
+    if(height <= 0 || height >= 3.00) {
+        console.log("Altura inválido.");
+        heightValid = false;
+        imc.textContent = "Altura inválido!"
+    }
+
+    if(heightValid && weightValid) {
+        var calcIMC = weight / (height * height);
+        imc.textContent = calcIMC.toFixed(2);
+    }
 }
-
-if(height <= 0 || height >= 3.00) {
-    console.log("Altura inválido.");
-    heightValid = false;
-    imc.textContent = "Altura inválido!"
-}
-
-if(heightValid && weightValid) {
-    var calcIMC = weight / (height * height);
-    imc.textContent = calcIMC;
-}
-
-
-
-
